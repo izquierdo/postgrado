ae5dadc7005152ac14af2ac7adbdf23dce272559
Daniel Izquierdo <daniel@maray.(none)>
2009-11-12 11:18:05 -0430

    samecompany generator

--- tesis/src/flights/flights.py
+++ tesis/src/flights/flights.py
@@ -51,15 +51,15 @@ def samecompany(argv):
     # generate query
     company_var = Argument('var', steps)
     query_pred = "q"
-    query_args = [Argument('var', i) for i in range(steps)] + [company_var]
+    query_args = [Argument('var', i) for i in range(1, steps+1)]
     query_table = Table(query_pred, query_args)
     cityA, cityB = (Argument('const', 1), Argument('const', 2))
-    query_sos = [Table(flights_pred, [cityA, Argument('var', 1), Argument('const', 0)])]
+    query_sos = [Table(flights_pred, [cityA, Argument('var', 1), company_var])]
 
     for i in range(1, steps-1):
-        query_sos.append(Table(flights_pred, [Argument('var', i), Argument('var', i+1), Argument('const', 0)]))
+        query_sos.append(Table(flights_pred, [Argument('var', i), Argument('var', i+1), company_var]))
 
-    query_sos.append(Table(flights_pred, [Argument('var', steps-1), cityB, Argument('const', 0)]))
+    query_sos.append(Table(flights_pred, [Argument('var', steps-1), cityB, company_var]))
 
     query = Query(query_table, query_sos)
 
@@ -67,8 +67,17 @@ def samecompany(argv):
     view_pred_prefix = 'v'
 
     def gen_view(i):
-        return 
-    views = []
+        varS = Argument('var', 1)
+        varT = Argument('var', 2)
+        constCompany = Argument('const', i)
+
+        view_table = Table(view_pred_prefix + str(i), [varS, varT])
+        view_sos = [Table(flights_pred, [varS, varT, constCompany])]
+        view = Query(view_table, view_sos)
+
+        return view
+
+    views = [gen_view(i) for i in range(100, 100+companies)]
 
     return query, views
 
@@ -98,7 +107,6 @@ def main():
     queryfile = open(queryfilename, 'w')
     queryfile.write(str(query) + '\n')
     queryfile.close()
-
 
 def usage_and_quit():
     print "Uso: ./buscawiki <archivo> (simple <regex> | relevancia <regex> | grafo <regex>)"
