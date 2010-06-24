f51bda1489ac52b5942274c2c78375ad010af603
Daniel Izquierdo <daniel@maray.(none)>
2010-06-23 21:37:24 -0430

    clase especializacion ontologias

--- tesis/src/omcdsat/mcdsat/Ontologies/ontologies.py
+++ tesis/src/omcdsat/mcdsat/Ontologies/ontologies.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from itertools import product
+from collections import defaultdict
+
 from Parser.CQparser import cargarCQ
 
 def load_ontology(filename):
@@ -8,17 +10,31 @@ def load_ontology(filename):
 
     return o
 
+class Spec:
+    def __init__(self, subclass_name, subclass_args, class_name, class_args):
+        self.subclass_name = subclass_name
+        self.class_name = class_name
+
+        self.mapping = defaultdict(set)
+
+        for i, arg in enumerate(subclass_args):
+            self.mapping[arg].add((0, i))
+
+        for i, arg in enumerate(class_args):
+            self.mapping[arg].add((1, i))
+
+    def __repr__(self):
+        return "%s [ %s (%s)" % (self.subclass_name, self.class_name, self.mapping)
+
 def generate_bichos(filename):
     o = load_ontology(filename)
 
     tc = set()
 
     for rel in o:
-        tc.add((rel.cabeza, rel.cuerpo[0]))
+        tc.add(Spec(rel.cabeza.predicado, rel.cabeza.orden, rel.cuerpo[0].predicado, rel.cuerpo[0].orden))
 
     # transitive closure
-
-    print tc
 
     while True:
         new = set()
