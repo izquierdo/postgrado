ecc3a180d7a77130a91275c94a6b98aff07e17a5
Daniel Izquierdo <daniel@maray.(none)>
2010-06-27 15:27:05 -0430

    generator de benchmark 0

--- tesis/benchmarks/omcdsat/0/generator.sh
+++ tesis/benchmarks/omcdsat/0/generator.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-QRPGEN=/home/daniel/proyectos/postgrado/tesis/src/qrpgen/qrpgen.py
+GEN=./generator.py
 
-for nquery_sos in `seq 2 1 20`; do
-    for nviews in `seq 10 10 50`; do
-        VIEWSFILE=casos/views-$nviews-$nquery_sos.in
-        QUERYFILE=casos/query-$nviews-$nquery_sos.in
-
-        $QRPGEN $VIEWSFILE $QUERYFILE flights.sameairline_manyviews $nviews $nquery_sos 2
-    done
+for levels in `seq 7`; do
+        $GEN $levels > casos/${levels}levels.ontology
 done
