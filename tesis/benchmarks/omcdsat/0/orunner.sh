da14ff362e34fedb3aaa116244bc8101303400f9
Daniel Izquierdo <daniel@maray.(none)>
2010-06-27 15:49:20 -0430

    runners de benchmark0

---
+++ tesis/benchmarks/omcdsat/0/orunner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GENONTOLOGIES="$1"
+OMCDSAT="$2"
+ONTOLOGY="$3"
+VIEWS="$4"
+QUERY="$5"
+
+${GENONTOLOGIES} $ONTOLOGY && \
+${OMCDSAT} RW ${VIEWS} ${QUERY} casos/costs.txt ${ONTOLOGY}.ontology > /dev/null
+true
--- tesis/benchmarks/omcdsat/0/runner.sh
+++ tesis/benchmarks/omcdsat/0/runner.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
 INDIR=casos
+OUT=omcdsat_0.raw
+TIME="timeout 600 /usr/bin/time -apo ${OUT}"
+GENONTOLOGIES=/home/daniel/proyectos/postgrado/tesis/src/omcdsat/mcdsat/GenOntologies.py
+OMCDSAT=/home/daniel/proyectos/postgrado/tesis/src/omcdsat/mcdsat/mcdsat
 
-OUT_MCD=plot_mcd7.raw
-OUT_MMS=plot_mms7.raw
-
-TIME_MCD="timeout 120 /usr/bin/time -apo ${OUT_MCD}"
-TIME_MMS="timeout 120 /usr/bin/time -apo ${OUT_MMS}"
-
-PROGRAM=/home/daniel/proyectos/postgrado/tesis/src/imcdsat/mcdsat/compileonly
-
-MCDSAT=/home/daniel/proyectos/postgrado/tesis/src/imcdsat/mcdsat/mcdsat
-MINIMAXSAT="/home/daniel/proyectos/postgrado/tesis/src/MiniMaxSat/minimaxsat1.0 -F=2"
-
-echo '################################################################################' | tee -a ${OUT_MCD}
 echo '################################################################################' | tee -a ${OUT_MMS}
 
-for nquery_sos in `seq 2 1 20`; do
-    for nviews in `seq 10 10 50`; do
-        VIEWSFILE=casos/views-$nviews-$nquery_sos.in
-        QUERYFILE=casos/query-$nviews-$nquery_sos.in
-        COSTFILE=../../../src/costs-pot2.txt
+for levels in `seq 2 3`; do
+    ONTOLOGY=casos/${levels}levels.ontology
+    VIEWS=casos/vistas.txt
+    QUERY=casos/query.txt
 
-        CNF=`basename $VIEWSFILE`.cnf
+    echo ${levels} | tee -a ${OUT}
 
-        echo ${nviews} ${nquery_sos} | tee -a ${OUT_MCD}
-        echo ${nviews} ${nquery_sos} | tee -a ${OUT_MMS}
-
-        for i in `seq 1`; do #n times to get average
-            ${TIME_MCD} ${PROGRAM} RW ${VIEWSFILE} ${QUERYFILE} > /dev/null
-
-            ${TIME_MMS} ./mmsrunner.sh "$MCDSAT" "$VIEWSFILE" "$QUERYFILE" "$COSTFILE" "$MINIMAXSAT" "$CNF"
-        done
+    for i in `seq 1`; do #n times to get average
+        ${TIME} ./orunner.sh "$GENONTOLOGIES" "$OMCDSAT" "$ONTOLOGY" "$VIEWS" "$QUERY"
     done
 done
