9dac18c7270507d98684790880f36279e5fb54c2
Daniel Izquierdo <daniel@maray.(none)>
2010-07-03 19:10:37 -0430

    commit para poder hacer switch branch

--- tesis/benchmarks/preferences/0/runner.sh
+++ tesis/benchmarks/preferences/0/runner.sh
@@ -4,18 +4,17 @@ INDIR=casos
 OUTFILE=preferences0.out
 
 TIME="/usr/bin/time -apo ${OUTFILE}"
-PROGRAM=/home/daniel/proyectos/postgrado/tesis/src/imcdsat/mcdsat/mcdsat
+PROGRAM=/home/daniel/proyectos/postgrado/tesis/src/imcdsat/mcdsat/compileonly
 
-#echo '########' | tee -a ${OUTFILE}
+echo '########' | tee -a ${OUTFILE}
 
-for vistas in `seq 5 5 5`; do
-    for pasos in `seq 2 2`; do
+for vistas in `seq 90 10 100`; do
+    for pasos in `seq 2 5`; do
             VIEWSFILE=${INDIR}/views-${vistas}-${pasos}.txt
             QUERYFILE=${INDIR}/query-${vistas}-${pasos}.txt
 
-            #echo ${vistas} ${pasos} ${copias} | tee -a ${OUTFILE}
+            echo ${vistas} ${pasos} | tee -a ${OUTFILE}
 
-            #${TIME} ${PROGRAM} RW ${VIEWSFILE} ${QUERYFILE}
-            ${PROGRAM} RW ${VIEWSFILE} ${QUERYFILE}
+            ${TIME} ${PROGRAM} RW ${VIEWSFILE} ${QUERYFILE} > /dev/null
     done
 done
